dc58b5f790ff35d6d2aa6f7215991cb06042b790
Tarsh <tarshtarsh333@gmail.com>
2021-11-20 17:10:59 +0530

    update

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ const auth = async (req, res, next) => {
     // console.log(token);
 
 
-    
-     const decodedData = jwt.verify(token, secret);
+
+    const decodedData = jwt.verify(token, secret);
     //  console.log(decodedData);
 
-      req.email = decodedData?.email;
-      req.id=decodedData?.id
-
+    req.email = decodedData?.email;
+    req.id = decodedData?.id
     next();
   } catch (error) {
     console.log(error);
@@ -36,27 +35,28 @@ const auth = async (req, res, next) => {
 };
 
 app.post('/signup', async (req, res) => {
-  const {name,email,password,phone,address,userLevel}=req.body
-// console.log(req.body);
+  const { name, email, password, phone, address, userLevel } = req.body
+  // console.log(req.body);
   try {
-    const olduser=await User.findOne({email:email})
+    const olduser = await User.findOne({ email: email })
     if (olduser) {
-      res.status(400).json({status:false,message:'Email already registered'})
+      res.status(400).json({ status: false, message: 'Email already registered' })
     }
-    else{
+    else {
       const hashedPassword = await bcrypt.hash(password, 12);
-      const newUser=await User.create({name,email,address,phone,password:hashedPassword,userLevel})
-      const token = jwt.sign( { email: newUser.email, id: newUser._id }, secret, { expiresIn: "3650d" } );
-      res.status(201).json({ id:newUser._id,token });
-    
-  }} catch (error) {
+      const newUser = await User.create({ name, email, address, phone, password: hashedPassword, userLevel })
+      const token = jwt.sign({ email: newUser.email, id: newUser._id, following: newUser.following }, secret, { expiresIn: "3650d" });
+      res.status(201).json({ id: newUser._id, token,following: newUser.following });
+
+    }
+  } catch (error) {
     console.log(error);
   }
-  }
+}
 )
 
-app.get('/',(req,res)=>{
-  res.json({hello:'hello'})
+app.get('/', (req, res) => {
+  res.json({ hello: 'hello' })
 })
 app.post('/signin', async (req, res) => {
   const { email, password } = req.body;
@@ -72,23 +72,68 @@ app.post('/signin', async (req, res) => {
 
     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "3650d" });
 
-    res.status(200).json({ id: oldUser._id,role:oldUser.role, token });
+    res.status(200).json({ id: oldUser._id, following: oldUser.following, token });
   } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
   }
 })
 
-app.post('/add-survey',auth,async(req,res)=>{
+app.post('/add-survey', auth, async (req, res) => {
   console.log(req.body);
-  const {survey,title,desc,link}=req.body
+  const { survey, title, desc, link } = req.body
   // console.log(req.body);
   try {
-    const newSurvey=await Survey.create({userId:req.id,questions:survey,description:desc,title,link})
+    const newSurvey = await Survey.create({ userId: req.id, questions: survey, description: desc, title, link })
     res.status(201).json({ newSurvey });
   } catch (error) {
     console.log(error);
-    res.json({error})
+    res.json({ error })
   }
 })
-
-app.listen(process.env.PORT || 5000);
+app.get('/members', auth, async (req, res) => {
+  try {
+    const members = await User.find({ userLevel: 1 })
+    // console.log(members);
+    res.status(201).json({ members });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error })
+  }
+})
+app.post('/unfollow', auth, async (req, res) => {
+  console.log("unfollow");
+  try {
+    const newUser = await User.updateOne({email:req.email}, { $pull: { following: { $in: req.body.id } } })
+    res.status(201).json({ success:"success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error })
+  }
+})
+app.post('/follow', auth, async (req, res) => {
+  // console.log(req.body);
+  console.log("follow");
+  try {
+    const newUser = await User.updateOne({email:req.email}, { $push: { following: req.body.id } })
+    // console.log(newUser);
+    res.status(201).json({ success:"success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error })
+  }
+})
+app.get('/surveys',auth,async(req,res)=>{
+  console.log(req.body);
+  try {
+    const user=await User.findOne({email:req.email})
+    // console.log(user.following);
+    const surveys=await Survey.find({userId:{$in:[...user.following]}})
+    // console.log(surveys);
+    res.status(201).json({ surveys});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error })
+  }
+})
+const port =process.env.PORT || 5000
+app.listen(port, () => console.log(`App listening on port ${port}!`));
